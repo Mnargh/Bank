3607caec63dee84580280c2905eae39c04444a0c
Tom Brandon <tom.j.v.brandon@gmail.com>
2018-01-29 11:39:19 +0000

    Can now deposit money into the account

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -7,4 +7,8 @@ describe Account do
   it "Initialises with an initial balance of 0" do
     expect(account.balance).to eq (0)
   end
+
+  it "Can deposit money in the account" do
+    expect{account.deposit(10)}.to change {account.balance}.from(0).to(10)
+  end
 end
