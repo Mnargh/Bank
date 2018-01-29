5267add3fa840ace6dca74e3458d1b1c2bf67a7d
Tom Brandon <tom.j.v.brandon@gmail.com>
2018-01-29 12:14:49 +0000

    Refactored account testing to initialise an empty account and one with a positive balance

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -2,23 +2,25 @@ require 'account'
 
 describe Account do
 
-  let(:account){Account.new}
+  let(:empty_account){Account.new}
+  let(:account){Account.new(10)}
+
 
   it "Initialises with an initial balance of 0" do
-    expect(account.balance).to eq (0)
+    expect(empty_account.balance).to eq (0)
   end
 
   it "Can deposit money in the account" do
-    expect{account.deposit(10)}.to change {account.balance}.from(0).to(10)
+    expect{empty_account.deposit(10)}.to change {empty_account.balance}.from(0).to(10)
   end
 
   it "Can withdraw money from the account" do
-    account.deposit(10)
     expect{account.withdraw(5)}.to change {account.balance}.from(10).to(5)
   end
 
   it "Cannot withdraw more money than is available in the account" do
-    account.deposit(10)
     expect{account.withdraw(11)}.to raise_error("Insufficient funds available in account. You have #{account.balance} remaining.")
   end
+
+  
 end
